d59667fef20bd87e6a1bab065c45f6e6eb48ab5a
PhilipKnp <philip.kn@gmx.de>
2020-05-07 15:03:09 +0200

    Challenge Complete

--- JavaScript Basic Algorithms and Data Structures/Basic Data Structures/Use-an-Array-to-Store-a-Collection-of-Data.js
+++ JavaScript Basic Algorithms and Data Structures/Basic Data Structures/Use-an-Array-to-Store-a-Collection-of-Data.js
@@ -1,2 +1,7 @@
-//this is a test
-console.log("test");
+//Basic Data Structures
+//Use an Array to Store a Collection of Data
+//Link to Challenge: https://bit.ly/2WdDV5r
+
+//Solution:
+let yourArray; // Change this line
+yourArray = ["test", 42, true, false, "hello world"];
